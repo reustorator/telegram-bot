e39288a2c07e7caba81f86e7b207350066059e7a
rnikonorov <rNikonorov@bigland.ru>
2024-05-22 21:53:18 +0300

    Миграции

---
+++ internal/storage/migrations/20240522184245_create_sources.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE sources
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    feed_url   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL default now(),
+    updated_at TIMESTAMP    NOT NULL DEFAULT now()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sources
+-- +goose StatementEnd
